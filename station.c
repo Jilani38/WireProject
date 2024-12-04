0f698de791fd994459d9d5bb892bbc3c09ba84d7
Jilani38 <mohamed.jilani38@gmail.com>
2024-12-04 04:30:55 +0100

    station.c

---
+++ station.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "station.h"
+
+StationData *load_station_data(const char *filename) {
+    // Lire les données à partir d'un fichier CSV
+    // Implémentation spécifique...
+}
+
+void free_station_data(StationData *data) {
+    // Libération des ressources associées
+}
+
+// Autres fonctions spécifiques...
